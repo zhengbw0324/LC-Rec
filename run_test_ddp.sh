a61673c44165b7b33e5ebf78d9b037c23aec3c9e
Bowen Zheng <57226275+zhengbw0324@users.noreply.github.com>
2024-03-20 13:02:09 +0800

    Update run_test_ddp.sh

--- run_test_ddp.sh
+++ run_test_ddp.sh
@@ -6,7 +6,7 @@ DATA_PATH=./data
 OUTPUT_DIR=./ckpt/$DATASET/
 RESULTS_FILE=./results/$DATASET/xxx.json
 
-torchrun --nproc_per_node=8 --master_port=4324 test_ddp.py \
+torchrun --nproc_per_node=8 --master_port=23324 test_ddp.py \
     --ckpt_path $CKPT_PATH \
     --dataset $DATASET \
     --data_path $DATA_PATH \
